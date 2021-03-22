54354bbb8a0292cb4c20cbdd5ba75fffd9ee16c0
Max Goodman <maximgoodman@gmail.com>
2021-03-22 16:02:41 -0500

    added resume, broke content encoding(?)

--- deploy_to_s3.py
+++ deploy_to_s3.py
@@ -30,19 +30,22 @@ def get_html_files():
     html_files += [each for each in listdir('.') if each.endswith('.html')]
     return html_files
 
-def upload_directory(files: list, s3, bucket):
+def upload_directory(files: list, s3, bucket, html: bool):
     for f in files:
         #preserve directory and file name 
         print(f"Uploading {f}...")
-        s3.meta.client.upload_file(f, bucket, f)
+        if html:
+            s3.meta.client.upload_file(f, bucket, f, ExtraArgs={'ContentType':'text/html'})
+        else:
+            s3.meta.client.upload_file(f, bucket, f)
         print(f"Uploaded {f}!")
 
 html = get_html_files()
 assets = get_assets(dirs)
 print('Updating html')
-upload_directory(html, s3, bucket_name)
+upload_directory(html, s3, bucket_name, True)
 print('Updated html')
 print('Updating assets')
 for k in assets:
-    upload_directory(assets[k], s3, bucket_name)
+    upload_directory(assets[k], s3, bucket_name, False)
 print('Updated assets')
